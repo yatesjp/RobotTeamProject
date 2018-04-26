bf7e5467b886c4ad05a56c9e1c6c758610347343
wangt2 <wangt2@rose-hulman.edu>
2018-04-26 11:19:05 -0400

    Rename

--- sandbox_motors_new/person2_motors.py
+++ sandbox_motors_new/person2_motors.py
@@ -1,7 +1,7 @@
 """
 Functions for SPINNING the robot LEFT and RIGHT.
-Authors: David Fisher, David Mutchler and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher, David Mutchler and Tianlin Wang.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implment spin_left_seconds, then the relevant part of the test function.
 #          Test and correct as needed.
@@ -28,6 +28,17 @@ def test_spin_left_spin_right():
       3. Same as #2, but runs spin_left_by_encoders.
       4. Same as #1, 2, 3, but tests the spin_right functions.
     """
+    ev3.Sound.speak("Drive using input").wait()
+
+    while True:
+        travtime = int(input("Enter a time to drive (seconds): "))
+        if travtime == 0:
+            print('See ya later!')
+            break
+        speed = int(input("Enter a speed for the motors (0 to 900 dps): "))
+        stopping = input("Enter a stop action for the motors (0 to 900 dps): ")
+
+        spin_left_seconds(travtime, speed, stopping)
 
 
 def spin_left_seconds(seconds, speed, stop_action):
@@ -36,6 +47,18 @@ def spin_left_seconds(seconds, speed, stop_action):
     where speed is between -100 (full speed spin_right) and 100 (full speed spin_left).
     Uses the given stop_action.
     """
+
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    assert left_motor.connected
+    assert right_motor.connected
+
+    left_motor.run_forever(speed_sp=speed)
+    right_motor.run_forever(speed_sp=-speed)
+    time.sleep(seconds)
+    left_motor.stop()
+    right_motor.stop(stop_action=stop_action)
 
 
 def spin_left_by_time(degrees, speed, stop_action):
