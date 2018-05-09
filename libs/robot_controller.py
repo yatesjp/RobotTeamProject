5e6f44461180262c189d3e4ee897d871ca5519d1
memerimh <memerimh@rose-hulman.edu>
2018-05-09 10:20:50 -0400

    forward move class

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -21,3 +21,16 @@ class Snatch3r(object):
     
     # TODO: Implement the Snatch3r class as needed when working the sandox exercises
     # (and delete these comments)
+
+    def __init__(self):
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        assert self.left_motor.connected
+        assert self.right_motor.connected
+
+    def forward(self, inches, speed=100, stop_action='brake'):
+        degrees = inches * (360/4.2)
+        self.left_motor.run_to_rel_pos(position_sp=degrees, speed_sp=speed*8, stop_action=stop_action)
+        self.right_motor.run_to_rel_pos(position_sp=degrees, speed_sp=speed*8, stop_action=stop_action)
+        self.left_motor.wait_while("running")
+        self.right_motor.wait_while("running")
