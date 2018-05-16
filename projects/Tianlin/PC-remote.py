b0c1b64b33265a50bda0bec84acc1c677a098e62
wangt2 <wangt2@rose-hulman.edu>
2018-05-16 10:03:12 -0400

    Done

--- projects/Tianlin/PC-remote.py
+++ projects/Tianlin/PC-remote.py
@@ -1,35 +1,10 @@
-#!/usr/bin/env python3
-"""
-This module is the mini-project for the MQTT unit.  This module will be running on your PC and communicating with the
-m5_ev3_remote_drive.py module that is running on your EV3 (you have to write that module too, but it's easier).
-Only the Tkinter GUI has been made for you.  You will need to implement all of the MQTT communication.  The goal is to
-have a program running on your computer that can control the EV3.
+"Authors: Tianlin Wang."
 
-You will need to have the following features:
-  -- Clickable drive direction buttons to drive forward (up), backwards (down), left, right, and stop (space)
-    -- Keyboard shortcut keys that behave the same as clicking the buttons (this has already been wired up for you)
-  -- An entry box for the left and right drive motor speeds.
-    -- If both become set to 900 all of the drive direction buttons will go fast, for example forward goes 900 900
-    -- If both become set to 300 all of the drive direction buttons will go slower, for example reverse goes -300 -300
-    -- If 500 then left does -500 500, which causes the robot to spin left (use half speed -250 250 if too fast)
-    -- If set differently to say 600 left, 300 right the robot will drive and arc, for example forward goes 600 300
-  -- In addition to the drive features there needs to be a clickable button for Arm Up and Arm Down
-    -- There also need to be keyboard shortcut for Arm Up (u) and Arm Down (j).  Arm calibration is not required.
-
-  -- Finally you need 2 buttons for ending your program:
-    -- Quit, which stops only this program and allows the EV3 program to keep running
-    -- Exit, which sends a shutdown message to the EV3, then ends it's own program as well.
-
-You can start by running the code to see the GUI, but don't expect button clicks to do anything useful yet.
-
-Authors: David Fisher and Tianlin Wang.
-"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
 import ev3dev.ev3 as ev3
 import time
-
 import mqtt_remote_method_calls as com
 
 
